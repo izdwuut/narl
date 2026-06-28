123a50210d6ef1909a08a065036a864e32520308
Bartosz Konikiewicz <izdwuut@gmail.com>
2026-06-28 20:43:40 +0200

    Remove curse effects from BackpackEntityFactory

--- src/game/model/entities/items/backpack/BackpackEntityFactory.ts
+++ src/game/model/entities/items/backpack/BackpackEntityFactory.ts
@@ -1,20 +1,13 @@
 import type { Component } from "../../../../../core/ecs/Component";
-import { addComponents, upsertComponents } from "../../../../../core/ecs/queries/component";
+import { addComponents } from "../../../../../core/ecs/queries/component";
 import { addEntities } from "../../../../../core/ecs/queries/entities";
-import { COLORS } from "../../../../../utils/colors";
 import { getDummyArray } from "../../../../../utils/getDummyArray";
 import { RNG } from "../../../../systems/rng/rng";
 import { NestDepthComponent } from "../../../components/containers/NestDepthComponent";
 import { SizeComponent } from "../../../components/containers/SizeComponent";
-import { ColorComponent } from "../../../components/display/ColorComponent";
-import { EquippableComponent } from "../../../components/eq/EquippableComponent";
 import { MainHandComponent } from "../../../components/eq/MainHandComponent";
-import { CursedComponent } from "../../../components/items/CursedComponent";
-import { DmgComponent } from "../../../components/items/DmgComponent";
-import { DmgModComponent } from "../../../components/items/DmgModComponent";
 import { DroppableComponent } from "../../../components/items/DroppableComponent";
 import { PickupableComponent } from "../../../components/items/PickupableComponent";
-import { isCursed } from "../../../queries/curse";
 import { PlaceholderEntity } from "../PlaceholderItemEntity";
 import { BackpackEntity } from "./BackpackEntity";
 
@@ -69,26 +62,5 @@ export class BackpackEntityFactory {
     const droppable = new DroppableComponent();
     addComponents(backpack, droppable);
     return backpack;
-  }
-
-  static curse(item: BackpackEntity): boolean {
-    const wasCursed = isCursed(item);
-    if (wasCursed) {
-      return false;
-    }
-    const components = [
-      new CursedComponent(),
-      new ColorComponent({ color: COLORS.CURSED }),
-      new DmgModComponent({ dmgMod: 0.5 }),
-      new EquippableComponent(),
-      new DmgComponent({ dmg: RNG.items.range(1, 3) }),
-    ];
-
-    upsertComponents(item, ...components);
-    return true;
-  }
-
-  static shouldBeCursed(item: BackpackEntity): boolean {
-    return !!item;
   }
 }
